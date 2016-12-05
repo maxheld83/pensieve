b875256b720b79f1014b76b8259bb8a611190a43
Maximilian Held <info@maxheld.de>
2016-12-05 18:38:39 +0100

    add more generics

--- R/check.R
+++ R/check.R
@@ -1,9 +1,24 @@
 #' @title Validate S3 classes from this package.
-#' @description Use \code{check} to validate S3 classed objects from this package.
+#' @description Use \code{check}, \code{test}, \code{assert} and \code{expect} to validate S3 classed objects from this package.
 #' @export
-#' @param x [any] class object created by respective \code{make_} function.
+#' @param x class object created by respective \code{make_} function.
 #' @family import helpers
 #' @family validation functions
 check <- function(x) {
   UseMethod("check")
+}
+
+#' @rdname check
+expect <- function(x) {
+  UseMethod("expect")
+}
+
+#' @rdname check
+test <- function(x) {
+  UseMethod("test")
+}
+
+#' @rdname check
+assert <- function(x) {
+  UseMethod("assert")
 }
