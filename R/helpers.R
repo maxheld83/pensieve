ae94d63f8ae84a0de2a02571539a9a4bdad955a3
Maximilian Held <info@maxheld.de>
2016-12-16 12:50:51 +0100

    fix style

--- R/helpers.R
+++ R/helpers.R
@@ -5,6 +5,8 @@ capitalize <- function(x) {
   s <- strsplit(x, " ")[[1]]
   paste(toupper(substring(s, 1,1)),
         substring(s, 2),
-        sep="",
-        collapse=" ")
+        sep = "",
+        collapse = " ")
 }
+
+
