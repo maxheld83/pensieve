2c5ede7a76cc1a78206254680581ac14124c3d6c
Maximilian Held <info@maxheld.de>
2017-01-10 21:41:04 +0100

    improve rendering, structure

--- book/_render.R
+++ book/_render.R
@@ -1,4 +1,11 @@
 setwd("book")
-bookdown::render_book(input = 'index.Rmd', output_format = 'bookdown::gitbook', output_dir = '../_book')
-bookdown::render_book(input = 'index.Rmd', output_format = 'bookdown::pdf_book', output_dir = '../_book')
-bookdown::render_book(input = 'index.Rmd', output_format = 'bookdown::epub_book', output_dir = '../_book')
+if (interactive()) {
+  bookdown::render_book(input = 'index.Rmd', output_format = 'bookdown::gitbook', output_dir = '../_book')
+  setwd("..")
+  rstudioapi::viewer("_book/index.html")
+} else {
+  bookdown::render_book(input = 'index.Rmd', output_format = 'bookdown::gitbook', output_dir = '../_book')
+  bookdown::render_book(input = 'index.Rmd', output_format = 'bookdown::pdf_book', output_dir = '../_book')
+  bookdown::render_book(input = 'index.Rmd', output_format = 'bookdown::epub_book', output_dir = '../_book')
+  setwd("..")
+}
