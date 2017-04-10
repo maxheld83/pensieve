71ca54e1fc87800a648af7d1b55cc4e5b2f74741
Maximilian Held <info@maxheld.de>
2017-04-10 12:26:27 +0200

    add boilerplate for score

---
+++ R/score.R
@@ -0,0 +1,68 @@
+# CALCULATION ====
+#' @title Factor scores.
+#'
+#' @description Calculates the factor scores as loadings-weighted averages of raw Q-Sorts (regression scores).
+#'
+#' @export
+#'
+#' @inheritParams QLoas
+#'
+#' @inheritParams QSorts
+#'
+#' @family analysis functions
+#' @family scoring functions
+#'
+#' @examples
+#' cors <- correlate(sorts = civicon_2014$qData$sorts[,,"before"])  # preparatory step
+#' loas <- extract(cors = cors, nfactors = 3, fa_type = "pca")  # preparatory step
+#' scores <- score(loas = loas, sorts = civicon_2014$qData$sorts[,,"before"])
+score <- function(loas, sorts) {
+  # Input validation ====
+
+  # Calculation ====
+  scores <- matrix(1,1)
+
+  return(scores)
+}
+
+# CLASS CONSTRUCTION ====
+#' @rdname score
+#'
+#' @export
+#'
+#' @template construct
+#'
+#' @param scores A numerical matrix with factor scores.
+#'
+#' @examples
+#' # this just assigns the class, without validation (not recommended)
+#' scores <- QScores(scores = scores, validate = FALSE)
+QScores <- function(scores, validate = TRUE) {
+  # assign class
+  scores <- classify_clever(x = scores, classname = "QScores")
+  assert_class2(x = scores, validate = validate)
+  return(scores)
+}
+
+
+#' @describeIn score validation
+#'
+#' @export
+#'
+#' @template check
+#'
+#' @examples
+#' # this validates the class
+#' check(x = scores)
+check.QScores <- function(x) {
+  res <- NULL
+
+  res$matrix <- check_matrix(x = x,
+                             mode = "numeric",
+                             any.missing = TRUE,
+                             all.missing = FALSE,
+                             row.names = "strict")
+  #TODO this needs more, such as the range of the raw data etc.
+
+  return(report_checks(res = res, info = "QScores"))
+}
