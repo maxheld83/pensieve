63fe2cf4c272d3ba1a4e6ad38363635768375d03
Maximilian Held <info@maxheld.de>
2017-07-25 13:31:06 +0200

    improve item rendering #282

--- R/items.R
+++ R/items.R
@@ -1,6 +1,239 @@
 # QItems ====
 # this ties it all together in a list, checks for consistency
 
+# QSet ====
+
+# QItem ====
+
+make_cards <- function(item_text,
+                       item_handle,
+                       template = "simple_rect.Rnw",
+                       output_dir = tempdir(),
+                       fontsize = "normalsize",
+                       language = NULL,
+                       paperwidth = 16,
+                       paperheight = 9,
+                       top = 1,
+                       bottom = 1,
+                       left = 1,
+                       right = 1,
+                       units = "cm") {
+
+  # Input validation
+  checkmate::assert_string(x = item_text, na.ok = FALSE, min.chars = 1, null.ok = FALSE)
+  checkmate::assert_string(x = item_handle, na.ok = FALSE, min.chars = 1, null.ok = FALSE)
+  checkmate::assert_choice(x = template, choices = c("simple_rect.Rnw"), null.ok = FALSE)
+  checkmate::assert_directory(x = output_dir, access = "w")
+  # other inputs are validated in downstream functions
+
+  # find template
+  in_build_path <- file.path("templates", "cards", template)
+  in_source_path <- file.path("inst", in_build_path)
+  # this is just to make debugging easier; if run from pensieve source dir, then use source template
+  if (file.exists(in_source_path)) {
+    input_path <- in_source_path
+  } else {
+    input_path <- file.path(system.file(package = "pensieve"), in_build_path)
+  }
+
+  # other prep
+  tex_file <- file.path(output_dir, paste0(item_handle, ".tex"))
+  asset_paths <- NULL
+
+  # render
+  knitr::knit(input = input_path, output = tex_file)
+  asset_paths$latex <- tex_file
+  withr::with_dir(new = output_dir, code = {
+    # changing wd with with_dir is sadly necessary because below utilities do not offer convenient output paths
+    tools::texi2pdf(file = tex_file)
+    asset_paths$pdf <- file.path(output_dir, paste0(item_handle, ".pdf"))
+    pdf2svg(pdf_input = paste0(item_handle, ".pdf"))
+    asset_paths$svg <- file.path(output_dir, paste0(item_handle, ".svg"))
+  })
+  return(asset_paths)
+}
+
+# helper to convert pdf to svg
+pdf2svg <- function(pdf_input) {
+  # Input validation
+  checkmate::assert_file_exists(x = pdf_input, extension = "pdf")
+  checkmate::assert_character(x = pdf_input, any.missing = FALSE, unique = TRUE)
+  checkmate::assert_os(os = c("mac", "linux"))
+
+  svg_output <- paste0(
+    tools::file_path_sans_ext(pdf_input),
+    ".svg"
+  )
+  system2(command = "pdf2svg",
+          args = c(pdf_input,
+                   svg_output,
+                   "1"),
+          stderr = "")  # only take 1st page
+}
+
+
+# helpers to create latex formatting instructions
+latex <- list(set = NULL, # these are the functions
+              options = NULL) # these are the available options
+
+# insert arbitrary fontsize
+
+latex$options$fontsize <- c(
+  # this list is from https://en.wikibooks.org/wiki/LaTeX/Fonts#Sizing_text
+  # must remain in ascending order!
+  "tiny",
+  "scriptsize",
+  "footnotesize",
+  "small",
+  "normalsize",
+  "large",
+  "Large",
+  "LARGE",
+  "huge",
+  "Huge"
+)
+
+latex$set$fontsize <- function(fontsize) {
+  # fontsize <- "tiny"
+  checkmate::assert_character(x = fontsize, any.missing = FALSE, len = 1)
+  checkmate::assert_choice(x = fontsize, choices = latex$options$fontsize, null.ok = FALSE)
+
+  cat("\\", fontsize, "\n", sep = "")
+}
+
+
+# add arbitrary babel invocation
+latex$options$babel <- c(
+  # this list is from https://tug.org/pracjourn/2007-1/gregorio/gregorio.pdf
+  "acadian",
+  "afrikaans",
+  "albanian",
+  "american",
+  "australian",
+  "austrian",
+  "bahasa",
+  "bahasai",
+  "bahasam",
+  "basque",
+  "brazil",
+  "brazilian",
+  "breton",
+  "british",
+  "bulgarian",
+  "canadian",
+  "canadien",
+  "catalan",
+  "croatian",
+  "czech",
+  "danish",
+  "dutch",
+  "english",
+  "esperanto",
+  "estonian",
+  "finnish",
+  "francais",
+  "french",
+  "frenchb",
+  "galician",
+  "german",
+  "germanb",
+  "greek",
+  "hebrew",
+  "hungarian",
+  "icelandic",
+  "indon",
+  "indonesian",
+  "interlingua",
+  "irish",
+  "italian",
+  "latin",
+  "lowersorbian",
+  "magyar",
+  "ngerman",
+  "norsk",
+  "nynorsk",
+  "polish",
+  "polutonikogreek",
+  "portuges",
+  "portuguese",
+  "romanian",
+  "russian",
+  "samin",
+  "scottish",
+  "serbian",
+  "slovak",
+  "slovene",
+  "spanish",
+  "swedish",
+  "turkish",
+  "ukrainian",
+  "uppersorbian",
+  "welsh",
+  "UKenglish",
+  "USenglish",
+  "malay",
+  "meyalu",
+  "naustrian",
+  "newzealand"
+)
+
+latex$set$babel <- function(language) {
+  checkmate::assert_character(x = language, any.missing = FALSE, len = 1)
+  checkmate::assert_choice(x = language, choices = latex$options$babel, null.ok = FALSE)
+
+  cat(
+    "\\usepackage[",
+    language,
+    "]{babel}",
+    "\n",
+    sep = ""
+  )
+}
+
+
+# set arbitrary dimensions
+latex$set$geometry <- function(paperwidth, paperheight, top, bottom, left, right, units) {
+  checkmate::assert_character(x = units, any.missing = FALSE, len = 1)
+  checkmate::assert_choice(x = units, choices = c("cm", "in"), null.ok = FALSE)
+
+  all_args <- as.list(environment())
+  num_args <- all_args[!(names(all_args) == "units")]
+
+  mapply(FUN = checkmate::assert_numeric,
+         x = num_args,
+         .var.name = names(num_args),
+         MoreArgs = list(
+           lower = 0,
+           finite = TRUE,
+           any.missing = FALSE,
+           len = 1,
+           null.ok = FALSE
+         )
+  )
+
+  opts <- sapply(
+    X = names(num_args),
+    FUN = function(x) {
+      return(paste0(
+        x,
+        "=",
+        num_args[[x]],
+        units
+      ))
+    }
+  )
+  opts <- c(opts, "vcentering", "hcentering")  # add vertical, horizontal center just to be safe
+
+  cat(
+    "\\usepackage[",
+    paste(opts, collapse = ", "),
+    "]{geometry}",
+    "\n",
+    sep = ""
+  )
+}
+
+
 # QItemSample ====
 # logical vector about the inclusion/exclusion of items
 # make this a function which *actually* samples
--- tests/testthat/test_items.R
+++ tests/testthat/test_items.R
@@ -2,17 +2,14 @@ context("Typsetting items")
 
 test_that(desc = "conversion from pdf to svg works", code = {
   skip_on_os(os = c("windows"))  # no easy way to get pdf2svg
-  checkmate::expect_file_exists(x = c("test1.pdf", "test2.pdf"))
-  pdf_input <- c(
-  "test1.pdf",
-  "test2.pdf"
-  )
+  pdf_input <- "test1.pdf"
+  checkmate::expect_file_exists(x = pdf_input)
   pdf2svg(pdf_input = pdf_input)
-  checkmate::expect_file_exists(x = c("test1.svg", "test2.svg"))
+  checkmate::expect_file_exists(x = "test1.svg")
 })
 
 test_that(desc = "pdf card is produced from string", code = {
   skip_on_appveyor()  # does not have latex
-  make_cards(text = "foo")
-  checkmate::expect_file_exists(x = "simple_rect.pdf")
+  output <- make_cards(item_text = "foo", item_handle = "foo_handle")
+  checkmate::expect_file_exists(x = output$pdf)
 })
