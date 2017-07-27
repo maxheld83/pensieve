7ff7768d4e95d11d2c209a9431899ba4e8cc8608
Maximilian Held <info@maxheld.de>
2017-07-27 22:43:38 +0200

    migrate, rename, reorg QItemConcourse

--- R/items.R
+++ R/items.R
@@ -1,9 +1,89 @@
 # QItems ====
 # this ties it all together in a list, checks for consistency
 
-# QSet ====
 
-# QItem ====
+# QItemConcourse ====
+#' @title Check and make QItemConcourse
+#'
+#' @description Check and make QItemConcourse
+#'
+#' @export
+#'
+#' @param concourse Character matrix of **full items**, with named rows as item handles and named columns as languages.
+#' Cells can be `NA` when full items are not available.
+#' Full items must be unique by columns.
+#'
+#' @template construct
+#'
+#' @family items QStudy-elements classes
+#'
+QItemConcourse <- function(concourse, validate = TRUE) {
+  assert_flag(x = validate,
+              na.ok = FALSE,
+              null.ok = FALSE)
+
+  concourse <- classify_clever(x = concourse, classname = "QConcourse")
+
+  assert_class2(x = concourse, validate = validate)
+
+  return(concourse)
+}
+
+#' @export
+#' @rdname check
+check.QItemConcourse <- function(x) {
+  res <- NULL
+
+  res$matrix <- check_matrix(x = x,
+                             mode = "character",
+                             any.missing = TRUE,
+                             all.missing = TRUE,
+                             row.names = "strict",
+                             col.names = "strict",
+                             null.ok = FALSE)
+
+  # check whether the concourse is all unique by columns, as is must be:
+  # makes no sense to have same item twice
+  # notice that being non-unique by row does not matter:
+  # conceivable, though unlikely, that items are same in two languages
+  res$unique_by_column <- check_unique_in_column(x = x)
+
+  return(report_checks(res = res, info = "QItemConcourse"))
+}
+
+#' @title Custom print method for knitr
+#'
+#' @description Provides custom print method for knitr.
+#' Can also be invoked manually to open interactive outputs in RStudio.
+#'
+#' @param x a character matrix with full item wording of class [`QItemConcourse`][QItemConcourse], as created by [QItemConcourse()].
+#'
+#' @template plot
+#'
+#' @inheritParams knitr::knit_print
+#'
+#' @export
+#'
+#' @family knitr output functions
+knit_print.QItemConcourse <- function(x, use_js = NULL, ...) {
+  # Input validation ====
+  use_js <- assert_n_infer_use_js(use_js = use_js)
+
+  x <- classify_clever(x = x, classname = "QItemConcourse")  # gotta make sure it IS QItems in the first place
+  assert(x)
+
+  # JS method ====
+  if (use_js) {  # interactive
+    DT::datatable(data = as.data.frame(x),
+                  options = list(searchHighlight = TRUE))
+  } else {
+    print(x)
+  }
+}
+
+
+
+# QSet ====
 
 make_cards <- function(item_text,
                        item_handle,
@@ -277,85 +357,6 @@ check.QItemStrata <- function(x) {
                            null.ok = FALSE)
   res <- c(res, check_named_array(x = x))  # via external helper
   return(report_checks(res = res, info = "QItemStrata"))
-}
-
-# QConcourse ====
-#' @title Check and make QConcourse
-#'
-#' @description Check and make QConcourse
-#'
-#' @export
-#'
-#' @param concourse A character matrix of full items, with named rows as item handles and named columns as languages.
-#' Cells can be `NA` when full items are not available.
-#' Full items must be unique by columns.
-#'
-#' @template construct
-#'
-#' @family import helpers
-#'
-QConcourse <- function(concourse, validate = TRUE) {
-  assert_flag(x = validate,
-              na.ok = FALSE,
-              null.ok = FALSE)
-
-  concourse <- classify_clever(x = concourse, classname = "QConcourse")
-
-  assert_class2(x = concourse, validate = validate)
-
-  return(concourse)
-}
-
-#' @export
-#' @rdname check
-check.QConcourse <- function(x) {
-  res <- NULL
-
-  res$matrix <- check_matrix(x = x,
-                             mode = "character",
-                             any.missing = TRUE,
-                             all.missing = TRUE,
-                             row.names = "strict",
-                             col.names = "strict",
-                             null.ok = FALSE)
-
-  # check whether the concourse is all unique by columns, as is must be:
-  # makes no sense to have same item twice
-  # notice that being non-unique by row does not matter:
-  # conceivable, though unlikely, that items are same in two languages
-  res$unique_by_column <- check_unique_in_column(x = x)
-
-  return(report_checks(res = res, info = "QConcourse"))
-}
-
-#' @title Custom print method for knitr
-#'
-#' @description Provides custom print method for knitr.
-#' Can also be invoked manually to open interactive outputs in RStudio.
-#'
-#' @param x a character matrix with full item wording of class [`QConcourse`][QConcourse], as created by [QConcourse()].
-#'
-#' @template plot
-#'
-#' @inheritParams knitr::knit_print
-#'
-#' @export
-#'
-#' @family knitr output functions
-knit_print.QConcourse <- function(x, use_js = NULL, ...) {
-  # Input validation ====
-  use_js <- assert_n_infer_use_js(use_js = use_js)
-
-  x <- classify_clever(x = x, classname = "QConcourse")  # gotta make sure it IS QItems in the first place
-  assert(x)
-
-  # JS method ====
-  if (use_js) {  # interactive
-  DT::datatable(data = as.data.frame(x),
-                options = list(searchHighlight = TRUE))
-  } else {
-    print(x)
-  }
 }
 
 
