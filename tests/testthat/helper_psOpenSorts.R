0c3921a310b77e489c574a14b80e589553c995da
Maximilian Held <info@maxheld.de>
2018-01-06 21:36:36 +0100

    refactor example #358

--- tests/testthat/helper_psOpenSorts.R
+++ tests/testthat/helper_psOpenSorts.R
@@ -1,31 +1,36 @@
 # create single open sort ====
 
-# Lisas open sort, matching by index
-assignments <- matrix(data = c(TRUE, FALSE, FALSE, TRUE),
-                      nrow = 2,
-                      dimnames = list(items = c("cat", "dog")))
-descriptions <- c("a pet which largely takes care of itself",
-                  NA)
+# Lisas open sort, matching descriptions by index
+assignments <- matrix(
+  data = c(TRUE, FALSE, FALSE, TRUE),
+  nrow = 2,
+  dimnames = list(items = c("cat", "dog")))
+descriptions <- c(
+  "a pet which largely takes care of itself",
+  NA  # dimension is assigned, but not described (not a problem)
+)
 lisa <- psOpenSort(assignments = assignments, descriptions = descriptions)
 
-# Peters open sort, matching by name
-assignments <- matrix(data = c(TRUE, TRUE, TRUE, FALSE, FALSE, FALSE),
-                      nrow = 2,
-                      dimnames = list(items = c("cat", "dog"),
-                                      categories = c("in_homes",
-                                                     "quiet",
-                                                     "herbivore")))
-descriptions <- c(in_homes = "Animal found in peoples homes.",
-                  quiet = "Does not make a lot of noise.",
-                  herbivore = "Eats plants.")
+# Peters open sort, descriptions are named, but still matched by index
+assignments <- matrix(
+  data = c(TRUE, TRUE, TRUE, FALSE, FALSE, FALSE),
+  nrow = 2,
+  dimnames = list(
+    items = c("cat", "dog"),
+    categories = c("in_homes", "quiet", "herbivore")
+))
+descriptions <- c(
+  in_homes = "Animal found in peoples homes.",
+  quiet = "Does not make a lot of noise.",
+  herbivore = "Eats plants.")  # defined, but never assigned (not a problem)
 peter <- psOpenSort(assignments = assignments, descriptions = descriptions)
 
 # Rebeccas open sort, without any descriptions provided
-assignments <- matrix(data = c(FALSE, FALSE, TRUE, TRUE),
-                      nrow = 2,
-                      dimnames = list(handles = c("cat", "dog")))
+assignments <- matrix(
+  data = c(FALSE, FALSE, TRUE, TRUE),
+  nrow = 2,
+  dimnames = list(handles = c("cat", "dog")))
 rebecca <- psOpenSort(assignments = assignments, descriptions = NULL)
-
 
 # now let's combine the individual sorts into a list ====
 open_sorts <- psOpenSorts(open_sorts = list(lisa = lisa, peter = peter, rebecca = rebecca))
