d426992d9fc2e0b604bb2c7307ced5019c6b253b
Maximilian Held <info@maxheld.de>
2018-01-06 19:07:39 +0100

    outsorce examples #358

--- R/psOpenSorts.R
+++ R/psOpenSorts.R
@@ -4,6 +4,7 @@
 #'
 #' @details
 #' Open sorting categorizations *cannot* be compared between participants, because each participants defines her own dimensions.
+#'
 #' **The canonical representation of open sorting data** is therefore a *list* of matrices, one for each participant.
 #' Every *individual* matrix is a [psOpenSort()] object, and together, they form a [psOpenSorts()] list.
 #' The rows in these matrices are the items, the columns are the dimensions, and cells are the assignment.
@@ -13,36 +14,7 @@
 #' A list of matrices, one for each participant.
 #' Matrices must be [psOpenSort()] object, or coercable to [psOpenSort()].
 #'
-#' @examples
-#' # Lisas open sort, matching by index
-#' assignments <- matrix(data = c(TRUE, FALSE, FALSE, TRUE),
-#'                       nrow = 2,
-#'                       dimnames = list(items = c("cat", "dog")))
-#' descriptions <- c("a pet which largely takes care of itself",
-#'                   NA)
-#' lisa <- psOpenSort(assignments = assignments, descriptions = descriptions)
-#'
-#' # Peters open sort, matching by name
-#' assignments <- matrix(data = c(TRUE, TRUE, TRUE, FALSE, FALSE, FALSE),
-#'                       nrow = 2,
-#'                       dimnames = list(items = c("cat", "dog"),
-#'                                       categories = c("in_homes",
-#'                                                      "quiet",
-#'                                                      "herbivore")))
-#' descriptions <- c(in_homes = "Animal found in peoples homes.",
-#'                   quiet = "Does not make a lot of noise.",
-#'                   herbivore = "Eats plants.")
-#' peter <- psOpenSort(assignments = assignments, descriptions = descriptions)
-#'
-#' # Rebeccas open sort, without any descriptions provided
-#' assignments <- matrix(data = c(FALSE, FALSE, TRUE, TRUE),
-#'                       nrow = 2,
-#'                       dimnames = list(handles = c("cat", "dog")))
-#' rebecca <- psOpenSort(assignments = assignments, descriptions = NULL)
-#' # providing no description is possible, but makes interpretation hard, if not meaningless.
-#'
-#' # now let's combine the individual sort into a list
-#' open_sorts <- psOpenSorts(open_sorts = list(lisa = lisa, peter = peter, rebecca = rebecca))
+#' @example tests/testthat/helper_psOpenSorts.R
 #'
 #' @return Object of class `psOpenSorts`.
 #'
