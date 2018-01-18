09b246a9d48022c0869196e3136c40f3953722ef
Maximilian Held <info@maxheld.de>
2018-01-18 19:59:27 +0100

    update to new komki data

--- tests/testthat/test_q-cat.R
+++ tests/testthat/test_q-cat.R
@@ -3,7 +3,7 @@ context("Co-occurence matrix generation")
 test_that(desc = "works with komki",
           code = {
   data("komki")
-  cooc <- count_cooccur(ass = komki$qcat$ass)
+  cooc <- count_cooccur(ass = komki$osorts)
   expect_array(x = cooc,
                mode = "integer",
                any.missing = TRUE,
@@ -11,22 +11,23 @@ test_that(desc = "works with komki",
                null.ok = FALSE)
   expect_integer(x = cooc,
                  lower = -1)
-  expect_equal(object = cooc["headturner", "the-better", "Valentine"],
+  expect_equal(object = cooc["kopf_verdrehen", "je_desto", "Valentine"],
                expected = 1,
-               info = "For `Valentine` `headturner` and `the-better` do not overlap 1 times.")
-  expect_equal(object = cooc["riddle", "eating-grandpa", "Jalena"],
+               info = "For `Valentine` `kopf_verdrehen` and `je_desto` do not overlap 1 times.")
+  expect_equal(object = cooc["nuschel", "opa_essen", "Jalena"],
                expected = 0,
-               info = "For `Jalena`, `riddle` and `eating-grandpa` do not overlap 0 times.")
-  expect_scalar_na(x = cooc["computer", "riddle", "Nhome"],
-                   null.ok = FALSE,
-                   info = "`Nhome` never saw `computer`, so `computer` and, say, `riddle` must be NA.")
-  expect_scalar_na(x = cooc["computer", "black-white", "Nhome"],
-                   null.ok = FALSE,
-                   info = "`Nhome` never saw `computer` or `black-white`, the two must be NA.")
-  expect_equal(object = unique(diag(cooc[,,"Thomas"])),
-               expected = 12,
-               info = "`Thomas` assigned and/or described 12 categories, so the diagonal should always be 12.")
+               info = "For `Jalena`, `nuschel` and `opa_essen` do not overlap 0 times.")
+  # these tests no longer work, b/c only 35 items are in current osorts
+  # expect_scalar_na(x = cooc["computer", "nuschel", "Nhome"],
+  #                  null.ok = FALSE,
+  #                  info = "`Nhome` never saw `computer`, so `computer` and, say, `nuschel` must be NA.")
+  # expect_scalar_na(x = cooc["computer", "schwarz_weiss", "Nhome"],
+  #                  null.ok = FALSE,
+  #                  info = "`Nhome` never saw `computer` or `schwarz_weiss`, the two must be NA.")
+  # expect_equal(object = unique(diag(cooc[,,"Thomas"])),
+  #              expected = 12,
+  #              info = "`Thomas` assigned and/or described 12 categories, so the diagonal should always be 12.")
   expect_equal(object = dim(cooc[,,"Thomas"]),
-               expected = c(41, 41),
-               info = "`Thomas` as everyone else had a maximum of 41 items, so that must be the extend of his matrix.")
+               expected = c(35, 35),
+               info = "`Thomas` as everyone else had a maximum of 35 items, so that must be the extend of his matrix.")
 })
