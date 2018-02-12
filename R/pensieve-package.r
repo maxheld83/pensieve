f2962535106e5a9a619df1c1377f5bf8324f1b71
Maximilian Held <info@maxheld.de>
2018-02-12 22:40:27 +0100

    add glue and purrr

--- R/pensieve-package.r
+++ R/pensieve-package.r
@@ -2,7 +2,7 @@
 #'
 #' @name pensieve
 #' @docType package
-#' @import ggplot2 checkmate testthat
+#' @import ggplot2 checkmate testthat glue
 #' @importFrom broom tidy
 #' @importFrom knitr knit_print
 "_PACKAGE"
