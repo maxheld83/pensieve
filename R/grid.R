31ad077358523a9b72d0142c2441a8bd46f538a6
Maximilian Held <info@maxheld.de>
2018-06-17 13:36:17 +0200

    add header and footer #381

--- R/grid.R
+++ R/grid.R
@@ -1,14 +1,16 @@
 # html5 grid ====
 
-write_html5grid <- function(grid, browsable = TRUE) {
+write_html5grid <- function(grid, browsable = TRUE, header = TRUE, footer = TRUE) {
   # input verification
   assert_flag(x = browsable, na.ok = FALSE, null.ok = FALSE)
+  assert_flag(x = header, na.ok = FALSE, null.ok = FALSE)
+  assert_flag(x = footer, na.ok = FALSE, null.ok = FALSE)
 
   # test dependencies
   requireNamespace2("htmltools")
 
   # gather HTML5 dependencies
-  dep <- htmltools::htmlDependency(
+  bs <- htmltools::htmlDependency(
     name = "bootstrap",
     version = "3.3.7",
     src = "inst/bootstrap-3.3.7-dist/",
@@ -19,11 +21,39 @@ write_html5grid <- function(grid, browsable = TRUE) {
 
   # create output
   output <- htmltools::tagList(
-    dep,
+    # dependencies
+    bs,
+
     htmltools::tags$table(
-      purrr::map(.x = rownames(grid), .f = function(rname) {
-        htmltools::tags$tr(write_html5grid_row(rowvec = grid[rname,]))
-      })
+
+      # header
+      if (header) {
+        htmltools::tags$thead(
+          htmltools::tags$tr(
+            purrr::map(.x = colnames(grid), .f = function(cname) {
+              htmltools::tags$th(cname)
+            })
+          )
+        )
+      },
+
+      # footer
+      if (footer) {
+        htmltools::tags$tfoot(
+          htmltools::tags$tr(
+            purrr::map(.x = colnames(grid), .f = function(cname) {
+              htmltools::tags$td(cname)
+            })
+          )
+        )
+      },
+
+      # body
+      htmltools::tags$tbody(
+        purrr::map(.x = rownames(grid), .f = function(rname) {
+          htmltools::tags$tr(write_html5grid_row(rowvec = grid[rname,]))
+        })
+      )
     )
   )
 
