28d64452a06a32efe380eaa5f6445317cf2dd811
Maximilian Held <info@maxheld.de>
2018-06-28 11:18:40 +0200

    factor out resizing #393

--- inst/htmlwidgets/inanimatus.js
+++ inst/htmlwidgets/inanimatus.js
@@ -17,16 +17,22 @@ HTMLWidgets.widget({
         // prepend html for table
         inan.prepend(createTable(x.grid, x.colnames, x.rownames, x.header, x.footer, x.aspect_ratio_cards));
         
-        // initial resizing
-        var availWidth = inan.innerWidth();
-        var nCols = Object.keys(x.colnames).length;
+        // define required vars
+        var nCols = Object.keys(x.rownames).length;
         var nRows = Object.keys(x.rownames).length;
-        var cardWidth = availWidth/nCols;
-        var cardHeight = cardWidth/x.aspect_ratio_cards;
-        var tableHeight = cardHeight * nRows;
-                
+        var aspectRatioCards = x.aspect_ratio_cards;
+        
+        // initial resizing
+        var tableAspRatio = findTableAspRatio(nCols, nRows, aspectRatioCards);
+        var newDims = resize2AspRatio(inan.innerWidth(), inan.innerHeight(), tableAspRatio);
+        
         // this is a side effect, but makes for cleaner DOM
-        document.querySelector('head').innerHTML += "<style>.ps-grid .cell {height:" + cardHeight + "px" + "}</style>";
+        // to avoid repeating this for every cell, we write it to the css in head, not every cell
+        var newCardHeight = Math.floor(newDims.newHeight/nRows) + "px";
+        document.querySelector('head').innerHTML += "<style>.ps-grid .cell {height:" + newCardHeight + "}</style>";
+        // there is only one such object, so we can write directly css inline
+        console.log(inan);
+        inan.find(".ps-grid").css("width", Math.floor(newDims.newWidth) + "px");
 
       },
 
--- inst/htmlwidgets/lib/inanimatus/inanimatus.js
+++ inst/htmlwidgets/lib/inanimatus/inanimatus.js
@@ -1,35 +1,33 @@
 // use strict mode to avoid problems, should apply only to this script
 "use strict";
 
-// run this on all window size change events
-$(window).bind("load resize orientationchange", function() {
-  resizer($(".ps-grid"));
-});
-
-function resizer(el) {
-  // this approach is inspired by https://www.html5rocks.com/en/tutorials/casestudies/gopherwoord-studios-resizing-html5-games/#disqus_thread
-  // to be safe, this SHOULD be recalculated every time, b/c el MIGHT have stuff which does not resize dynamically, therefore altering aspect ratio
-  var reqAspRatio = findAspectRatio(el);
-  // this should respect parent width
-  var availWidth = el.parent().innerWidth();
-  // but for height, we only care about viewport limitations; users will have to scroll accordingly
-  var availHeight = $(window).innerHeight();
+function resize2AspRatio(availWidth, availHeight, reqAspRatio) {
   var availAspRat = availWidth / availHeight;
-
-  // remember that we ONLY write out width, rest is dealt with via css padding hack
+  
+  // default value is whatever is given
+  var newWidth = availWidth;
+  var newHeight = availHeight;
+  
+  // this approach is inspired by https://www.html5rocks.com/en/tutorials/casestudies/gopherwoord-studios-resizing-html5-games/#disqus_thread
   if (availAspRat > reqAspRatio) {
     // this is e.g. 4/3 in 16/9, needs boxes on the side, aka width based on height
-    el.css("width", Math.floor(availHeight * reqAspRatio));
+    newWidth = availHeight * reqAspRatio;
   } else if (availAspRat < reqAspRatio) {
-    // this is e.g. 16/9 in 4/3, needs boxes on the top, aka, width can be max
-    el.css("width", Math.floor(availWidth));
+    // this is e.g. 16/9 in 4/3, needs boxes on the top, aka, height based on width
+    newHeight = availWidth / reqAspRatio;
   }
+  
+  var newDims = {
+    newWidth: newWidth,
+    newHeight: newHeight
+  };
+  
+  return newDims;
 }
 
-function findAspectRatio(el) {
-  // we're letting the browser figure out the correct aspect ratio of the *overall* grid, based on the padding-hack in the css
-  var aspRatio = el.outerWidth() / el.outerHeight();
-  return aspRatio;
+function findTableAspRatio(nCols, nRows, aspectRatioCards) {
+  var tableAspRatio = nCols/nRows * aspectRatioCards;
+  return tableAspRatio;
 }
 
 // create table from json
@@ -57,7 +55,7 @@ function createTable(tableData, colNames, rowNames, header, footer, aspectRatioC
   }
   
   table.appendChild(tableBody);
-  return table
+  return table;
 }
 
 function createTableRow(rowData, header, footer) {
