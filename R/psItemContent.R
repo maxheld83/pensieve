62278d434c69f1a3f995623f0432ab92e72db9bc
Maximilian Held <info@maxheld.de>
2018-08-09 14:51:27 +0200

    render entire chain of conversions

--- R/psItemContent.R
+++ R/psItemContent.R
@@ -265,12 +265,65 @@ validate_S3.psItemContentBin <- function(x, ...) {
 
 # rendering ====
 #' @title
-#' Render text items.
+#' Render a list of text items to desired format.
 #'
 #' @description
-#' Renders character vectors of items to pdf, svg and R graphics (grid).
+#' Goes through the conversion chain as long as necessary to return the desired output.
 #'
-#' @details
+#' @inheritParams psItemContent
+#' @eval document_choice_arg(arg_name = "format", choices = item_output_formats, before = "giving the output format to render items in.", default = "pdf")
+#'
+#' @keywords internal
+#'
+#' @return `[list()]`
+#' of vectors in output format (`[character()]` or `[raw()]`), one list element per item.
+render_items <- function(items,
+                         format = "tex") {
+  items <- as_psItemContent(obj = items)
+  assert_choice(x = format, choices = item_output_formats)
+
+  # capture formatting options
+  lang <- attr(x = items, which = "lang")
+  geometry_opts <- attr(x = items, which = "geometry_opts")
+  alignment <- attr(x = items, which = "alignment")
+
+  # how many steps need to be done?
+  n_steps <- which(item_output_formats == format)
+
+
+  purrr::imap(
+    .x = items,
+    .f = function(wording, handle) {
+      step <- 1
+      tex <- md2tex_mem(x = wording, path_in = handle, alignment = alignment)
+      step <- step + 1
+      if (step > n_steps) {
+        return(tex)
+      }
+      pdf <- texi2pdf2_mem(x = tex, path_in = handle)
+      step <- step + 1
+      if (step > n_steps) {
+        return(pdf)
+      }
+      svg <- pdf2svg_mem(x = pdf, path_in = handle)
+      step <- step + 1
+      if (step > n_steps) {
+        return(svg)
+      }
+      svg2grob_mem(x = svg, path_in = handle)
+    }
+  )
+}
+item_output_formats <- c("tex", "pdf", "svg", "grob")
+# these must stay in the order of the conversion chain!
+
+
+# export method ====
+#' @describeIn psItemContent Export rendered text items to pdf or svg.
+#' @inheritParams export_ps
+#' @inheritParams render_items
+#'
+#' @section Rendering items:
 #' It is often helpful to have a canonical, typeset version of text items, ready for for printing, web publishing or interpretation.
 #' Rendered text items should meet several criteria:
 #' - They should always fit on the *same* card size, both for practical reasons and to emphasize the equal "significance" of items, even if they are of different length.
@@ -287,127 +340,17 @@ validate_S3.psItemContentBin <- function(x, ...) {
 #'     Items are now fully available to the R Graphics system and can be used wherever [graphics::plot()] (or, to be precise, [grid::grid.draw()]) works.
 #'
 #' At each step of this necessary, but rather long conversion pipeline more (system) dependencies are required and asserted.
-#'
 #' Additionally, because some of the intermediary formats cannot be easily or fully converted, downstream outputs may be faulty.
+#'
 #' Always use the *earliest* possible output from the above conversion pipeline to maximize fidelity to the original PDF.
 # TODO mention that user-facing functions such as plot and knit_print automatically do this as far as possible.
 # TODO explain how you can write stuff to disc
 # TODO explain caching
 #'
-#' When `items` are named with item handles, such handles are used as file and object names.
-#'
 #' Because items always have to fit on one page, this function errors out when the rendered item would fill more than one page.
-#'
-#' @inheritParams psItemContent
-#' @inheritParams md2tex
-#' @inheritParams wrap_in_latex_env
-#' @inheritParams declare_pandoc_geometry
-#'
-#' @param tex `[list(character())]` giving a list of manually produced LaTeX markup, one for each `items`.
-#' Defaults to `NULL`, in which case the LaTeX markup is rendered automatically (recommended).
-#'
-#' @param pdf `[list(raw())]` giving a list of manually produced PDFs, one for each `items`.
-#' Defaults to `NULL`, in which case the PDF is rendered automatically (recommended).
-#'
-#' @param svg `[list(raw())]` giving a list of manually produced SVGs, one for each `items`.
-#' Defaults to `NULL`, in which case the SVG is rendered automatically (recommended).
-#'
-#' @param grob `[list(grob())]` giving a list of manually produced grobs, one for each `items`.
-#' Defaults to `NULL`, in which case the grob is rendered automatically (recommended).
-#'
-#' @export
-render_items <- function(items,
-                         fontsize_local = "tiny",
-                         fontsize_global = "10pt",
-                         alignment = "justified",
-                         lang = NULL,
-                         paperwidth = 8.5,
-                         paperheight = 5.4,
-                         top = 0.5,
-                         bottom = 0.5,
-                         left = 0.5,
-                         right = 0.5,
-                         unit = "cm",
-                         vcentering = TRUE,
-                         hcentering = TRUE,
-                         tex = NULL,
-                         pdf = NULL,
-                         svg = NULL,
-                         grob = NULL) {
-
-  # do items coercion here
-
-  # input validation
-  purrr::walk(.x = list(tex = tex, pdf = pdf, svg = svg, grob = grob), .f = function(x) {
-    assert_list(x = x, any.missing = FALSE, unique = TRUE, null.ok = TRUE, len = length(items))
-  })
-  purrr::walk(.x = tex, .f = function(x) {
-    assert_character(x = x, any.missing = FALSE, null.ok = TRUE)
-  })
-  purrr::walk(.x = pdf, .f = function(x) {
-    if (!is.null(x)) assertTRUE(x = is.raw(x))
-  })
-  purrr::walk(.x = svg, .f = function(x) {
-    if (!is.null(x)) assertTRUE(x = is.raw(x))
-  })
-  purrr::walk(.x = grob, .f = function(x) {
-    if (!is.null(x)) assertTRUE(x = grid::is.grob(x))
-  })
-
-  # check dependencies
-  requireNamespace2(x = "progress")
-
-  # md2tex ====
-  # below function requires these as a list
-  if (is.null(tex)) {
-    message("Step 1/4: Conversion from Markdown to LaTeX.")
-    tryCatch(
-      expr = {
-        # set up progress bar
-        pb <- progress::progress_bar$new(
-          total = 100,
-          format = "converting :name [:bar] :percent eta: :eta")
-        pb$tick(0) # start with 0 before first compute
-        tex <- purrr::imap(
-          .x = as.list(items),
-          .f = function(x, name, pb) {
-            pb$tick(tokens = list(name = name))
-            x <- wrap_in_latex_fontsize(tex = x, fontsize_local = fontsize_local)
-            x <- wrap_in_latex_alignment(tex = x, alignment = alignment)
-            md2tex_mem(
-              x = x,
-              path_in = fs::path_ext_set(path = name, ext = "md"),
-              fontsize_global = fontsize_global,
-              lang = lang
-            )
-          },
-          pb
-        )
-        pb$terminate()
-      },
-      error = function(cnd) {
-        tex <- NULL  # if one compilation fails, set all to NULL
-        warning(
-          glue::glue("Skipping conversion to LaTeX: ", conditionMessage(cnd)),
-          call. = FALSE
-        )
-      }
-    )
-  } else {
-    message("Skipping conversion to LaTeX: Using user-supplied LaTeX markup from 'tex'.")
-  }
-
-  list(tex = as.list(tex))
-}
-
-
-# export ====
-#' @describeIn psItemContent Export rendered text items to pdf or svg.
-#' @inheritParams export_ps
-#' @eval document_choice_arg(arg_name = "format", choices = item_output_formats, before = "giving the output format to render items in.", default = "pdf")
 export_ps.psItemContentText <- function(x, dir = ".", overwrite = FALSE, format = "pdf") {
   assert_S3(x)
-  assert_choice(x = format, choices = item_output_formats)
+  # render_items(items = x, format = format)
   res <- list(foo = "foo", bar = "bar")  # placeholder
   purrr::imap_chr(
     .x = res,
@@ -425,4 +368,3 @@ export_ps.psItemContentText <- function(x, dir = ".", overwrite = FALSE, format 
     }
   )
 }
-item_output_formats <- c("pdf", "svg")
