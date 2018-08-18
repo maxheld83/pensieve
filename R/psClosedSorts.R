43dcedf956e1ceb320586cf3d486399255092a25
Maximilian Held <info@maxheld.de>
2018-08-18 15:25:56 +0200

    delete presorts
    this really has no place in pensieve

--- R/psClosedSorts.R
+++ R/psClosedSorts.R
@@ -1,47 +1,4 @@
-# QPreSorts ====
-
-#' @title Check and make QPreSorts
-#'
-#' @export
-#'
-#' @description Checks and makes QPreSorts
-#'
-#' @param presorts
-#' An integer matrix, with named rows as item handles, named columns as participant names and cells as presorts.
-#' `-1L` for `negative`, `0L` for `neutral` and `1L` for `positive`.
-#'
-#' @template construct
-#'
-#' @note
-#' `presorts` are stored as `integer()` because R does not allow factor matrices.
-#' Pre-sorting piles are, of course, `categorical` information and should be treated as such.
-#'
-#' @family import helpers
-QPreSorts <- produce_class_constructor(classname = "QPreSorts", fun = function(presorts) {
-  return(presorts)
-})
-
-#' @export
-#' @rdname check
-check.QPreSorts <- function(x) {
-  res <- NULL  # appease R
-
-  res$matrix <- check_matrix(x = x,
-                             mode = "integer",
-                             any.missing = TRUE,
-                             all.missing = FALSE,
-                             row.names = "strict",
-                             col.names = "strict")
-  res$range <- check_integer(x = x,
-                             any.missing = TRUE,
-                             lower = -2,
-                             upper = 2)
-
-  return(report_checks(res = res, info = "QPreSorts"))
-}
-
-
-# QSorts ====
+# helper ====
 #' @title Check and make QSorts
 #'
 #' @export
