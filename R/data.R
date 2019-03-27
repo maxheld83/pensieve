ee59ed2d8ec6713d8c5a7b7166266e2afe06fc2c
Maximilian Held <info@maxheld.de>
2019-03-27 19:54:36 +0100

    add data and field report for pairwise alpha test

--- R/data.R
+++ R/data.R
@@ -18,3 +18,14 @@
 # TODO still document this once it is sorted out
 #' @source \url{https://www.erziehungswissenschaften.hu-berlin.de/de/institut/mitarbeiter/1688111}
 "komki"
+
+#' @title Pairwise assessments of items on labor and digital technology
+#'
+#' @description
+#' A dataset of 18 participants, assessing all pairs of 11 items.
+#' Participants are experts on industrial relations and technology assembled in March 2019 at the Hans-Böckler Stiftung in Dusseldorf, Germany.
+#' This study was an alpha test of the pairwise methodology, conducted as part of the [denkzeug](http://datascience.phil.fau.de/denkzeug/) research project at the Friedrich-Alexander Universität Erlangen-Nürnberg, Germany.
+#'
+#' @format An array of item x item x persons.
+#' @source \url{http://datascience.phil.fau.de/denkzeug/}
+"hbs_alpha"
